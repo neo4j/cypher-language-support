fc9dc48c8bbbc5398b70d82810bd43ff79ccf40d
Nacho Cordón <nacho.cordon@neotechnology.com>
2022-12-26 15:50:50 +0100

    Adds first attempt at semantic analysis

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,13 @@ import {
 	Position,
 	CompletionItem,
 	ProposedFeatures,
-	InitializeParams
+	InitializeParams,
+	SemanticTokensRegistrationType,
+	DidChangeConfigurationNotification,
+	SemanticTokensRegistrationOptions,
+	SemanticTokens,
+	SemanticTokensBuilder,
+	SemanticTokensLegend
 } from 'vscode-languageserver/node';
 
 import { 
@@ -37,18 +43,116 @@ import {
 } from "antlr4ts";
 
 import { 
+	ParseTreeListener
+} from "antlr4ts/tree/ParseTreeListener"
+
+import { 
+	TerminalNode
+} from "antlr4ts/tree"
+
+import { 
 	CypherLexer 
 } from './antlr/CypherLexer';
 
 import { 
-	CypherParser 
+	CypherParser,
+	OC_LabelNameContext,
+	OC_LiteralContext,
+	OC_MatchContext,
+	OC_PropertyKeyNameContext,
+	OC_ReturnContext,
+	OC_VariableContext,
+	OC_WhereContext
 } from './antlr/CypherParser';
 
+import {
+	CypherListener,
+} from './antlr/CypherListener'
+import { debug } from 'console';
+
+
+const tokenTypesMap = new Map<string, number>();
+const tokenModifiersMap = new Map<string, number>();
+
+// ************************************************************
+// Part of the code that does the highlighting
+// ************************************************************
+class Legend implements SemanticTokensLegend {
+	tokenTypes: string[] = [];
+    tokenModifiers: string[] = [];
+
+	constructor() {
+		const tokenTypesLegend = [
+			'comment', 'string', 'keyword', 'number', 'regexp', 'operator', 'namespace',
+			'type', 'struct', 'class', 'interface', 'enum', 'typeParameter', 'function',
+			'method', 'decorator', 'macro', 'variable', 'parameter', 'property', 'label'
+		];
+		tokenTypesLegend.forEach((tokenType, index) => {
+			this.tokenTypes.push(tokenType)
+			tokenTypesMap.set(tokenType, index)
+		});
+
+		const tokenModifiersLegend = [
+			'declaration', 'documentation', 'readonly', 'static', 'abstract', 'deprecated',
+			'modification', 'async'
+		];
+		tokenModifiersLegend.forEach((tokenModifier, index) => {
+			this.tokenModifiers.push(tokenModifier)
+			tokenModifiersMap.set(tokenModifier, index)
+		});
+
+	}
+}
+
+const legend = new Legend;
+
+interface IParsedToken {
+	line: number;
+	startCharacter: number;
+	length: number;
+	tokenType: string;
+}
+
+class DocumentSemanticTokensProvider implements DocumentSemanticTokensProvider {
+	async provideDocumentSemanticTokens(textDocument: TextDocument): Promise<SemanticTokens> {
+		const lineText: string = textDocument.getText();
+		const inputStream = new ANTLRInputStream(lineText);
+		const lexer = new CypherLexer(inputStream);
+		const tokenStream = new CommonTokenStream(lexer);
+		
+		const parser = new CypherParser(tokenStream);
+		
+		const syntaxHighliter = new SyntaxHighlighter();
+		parser.addParseListener(syntaxHighliter as ParseTreeListener);
+		const tree = parser.oC_Cypher();
+
+		const builder = new SemanticTokensBuilder();
+		const sortedTokens = syntaxHighliter.allTokens.sort((a, b) => a.startCharacter - b.startCharacter)
+		
+		// When we push to the builder, tokens need to be sorted in ascending starting position
+		// i.e. as we find them when we read them from left to right in the line
+		sortedTokens.forEach((token) => {
+			// Nacho: FIXME The 0 index for the token modifiers at the end is hardcoded
+			const index = this._encodeTokenType(token.tokenType)
+			builder.push(token.line, token.startCharacter, token.length, index, 0);
+		});
+		return builder.build();
+	}
+
+	private _encodeTokenType(tokenType: string): number {
+		if (tokenTypesMap.has(tokenType)) {
+			return tokenTypesMap.get(tokenType)!;
+		}
+		return 0;
+	}
+}
 
 const connection = createConnection(ProposedFeatures.all);
 
 // Create a simple text document manager.
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+const semanticTokensProvider = new DocumentSemanticTokensProvider();
 
 connection.onInitialize((params: InitializeParams) => {
 	const result: InitializeResult = {
@@ -57,6 +161,14 @@ connection.onInitialize((params: InitializeParams) => {
 			// Tell the client that this server supports code completion.
 			completionProvider: {
 				resolveProvider: true
+			},
+			semanticTokensProvider: {
+				documentSelector: null,
+				legend: legend,
+				range: false,
+				full: {
+					delta: false
+				}
 			}
 		}
 	};
@@ -71,8 +183,121 @@ documents.onDidClose(e => {
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
+	semanticTokensProvider.provideDocumentSemanticTokens(change.document);
 	validateTextDocument(change.document);
 });
+
+connection.onInitialized(() => {
+	connection.client.register(DidChangeConfigurationNotification.type, undefined);
+    
+    const registrationOptions: SemanticTokensRegistrationOptions = {
+        documentSelector: null,
+        legend: legend,
+        range: false,
+        full: {
+            delta: false
+        }
+    };
+    connection.client.register(SemanticTokensRegistrationType.type, registrationOptions);
+});
+
+connection.languages.semanticTokens.on((params) => {
+    const document = documents.get(params.textDocument.uri);
+    if (document == undefined)
+        return { data: [] };
+
+    return semanticTokensProvider.provideDocumentSemanticTokens(document);
+});
+
+
+class SyntaxHighlighter implements CypherListener {
+	allTokens: IParsedToken[] = []
+	
+	enterOC_LabelName(ctx: OC_LabelNameContext) {
+		const token = ctx.start
+		this.allTokens.push({
+			line: token.line - 1,
+			startCharacter: token.startIndex,
+			length: token.stopIndex - token.startIndex + 1,
+			tokenType: "typeParameter"
+		});
+	}
+
+	enterOC_Return(ctx: OC_ReturnContext) {
+		const token = ctx.start
+		this.allTokens.push({
+			line: token.line - 1,
+			startCharacter: token.startIndex,
+			length: token.stopIndex - token.startIndex + 1,
+			tokenType: "keyword"
+		});
+	}
+	
+	exitOC_Match(ctx: OC_MatchContext) {
+		const opt = ctx.OPTIONAL()
+		const match = ctx.MATCH()
+
+		if (opt) {
+			const optToken = opt.symbol
+			this.allTokens.push({
+				line: optToken.line - 1,
+				startCharacter: optToken.startIndex,
+				length: optToken.stopIndex - optToken.startIndex + 1,
+				tokenType: "decorator"
+			});
+		}
+
+		const matchToken = match.symbol
+		this.allTokens.push({
+			line: matchToken.line - 1,
+			startCharacter: matchToken.startIndex,
+			length: matchToken.stopIndex - matchToken.startIndex + 1,
+			tokenType: "method"
+		});
+	}
+
+	enterOC_Variable(ctx: OC_VariableContext) {
+		const token = ctx.start
+		this.allTokens.push({
+			line: token.line - 1,
+			startCharacter: token.startIndex,
+			length: token.stopIndex - token.startIndex + 1,
+			tokenType: "variable"
+		});
+	}
+
+	enterOC_Where(ctx: OC_WhereContext) {
+		const token = ctx.start
+		this.allTokens.push({
+			line: token.line - 1,
+			startCharacter: token.startIndex,
+			length: token.stopIndex - token.startIndex + 1,
+			tokenType: "keyword"
+		});
+	}
+
+	enterOC_PropertyKeyName(ctx: OC_PropertyKeyNameContext) {
+		// FIXME Is this correct in this case for all cases, not just simple properties?
+		const token = ctx.start
+		this.allTokens.push({
+			line: token.line - 1,
+			startCharacter: token.startIndex,
+			length: token.stopIndex - token.startIndex + 1,
+			tokenType: "property"
+		});
+	}
+
+	
+	enterOC_Literal(ctx: OC_LiteralContext) {
+		const token = ctx.start
+		this.allTokens.push({
+			line: token.line - 1,
+			startCharacter: token.startIndex,
+			length: token.stopIndex - token.startIndex + 1,
+			tokenType: "string"
+		});
+	}
+}
 
 // ************************************************************
 // Part of the code that highlights the syntax errors
