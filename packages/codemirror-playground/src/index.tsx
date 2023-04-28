372d7d5c5b3975a915004d0b27387c3dcd8af676
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2023-04-28 18:14:31 +0200

    reset pkg code

--- packages/codemirror-playground/src/index.tsx
+++ packages/codemirror-playground/src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
-console.log("tes")
 
 createRoot(document.getElementById('root')).render(<App />);
