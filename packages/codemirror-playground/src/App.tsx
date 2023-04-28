4055a4cf522c55053dbfa16587ff68d0621ed461
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2023-04-28 17:36:39 +0200

    singleline

--- packages/codemirror-playground/src/App.tsx
+++ packages/codemirror-playground/src/App.tsx
@@ -4,10 +4,7 @@ import { TokenTable } from './TokenTable';
 
 export function App() {
   const [value, setValue] = useState(
-    `MATCH (n:Person)
-WHERE n.name = "Steve" 
-RETURN n 
-LIMIT 12;`,
+    `MATCH (n:Person) WHERE n.name = "Steve" RETURN n LIMIT 12;`,
   );
 
   return (
