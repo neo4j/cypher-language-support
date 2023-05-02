fd1276d8700756b8c6398cc1fb6b821365010c07
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2023-05-02 13:12:14 +0200

    switch node polyfills

--- packages/codemirror-playground/vite.config.ts
+++ packages/codemirror-playground/vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   // antlr4 build reference some node features that are not available in the browser
