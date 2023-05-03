d4290d236227814bf542e1fbf64f776d018c83e4
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2023-05-03 13:17:45 +0200

    no-minify

--- packages/codemirror-playground/vite.config.ts
+++ packages/codemirror-playground/vite.config.ts
@@ -5,4 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 export default defineConfig({
   // antlr4 build reference some node features that are not available in the browser
   plugins: [react(), nodePolyfills()],
+  build: { minify: false },
 });
