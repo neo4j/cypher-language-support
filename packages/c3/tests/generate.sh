c5a4c09ea5a4c8ebfc1ff41fec1efdfe792cb56c
Nacho Cordón <nacho.cordon@neotechnology.com>
2023-05-26 00:06:25 +0100

    Makes modifications for c3 to compile and pass the tests

--- packages/c3/tests/generate.sh
+++ packages/c3/tests/generate.sh
@@ -1,6 +1,6 @@
 ANTLR_JAR_FILE=/usr/local/lib/antlr4-tool.jar
 GRAMMAR_DIR=${PWD}/tests
-DOCKER_IMG=any0ne22/antlr4:4.12.0
+DOCKER_IMG=any0ne22/antlr4:4.13.0
 JAVA_ARGS="-Xmx500M -cp ${ANTLR_JAR_FILE} org.antlr.v4.Tool -Dlanguage=TypeScript"
 DOCKER_ARGS="run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v ${GRAMMAR_DIR}:/work ${DOCKER_IMG} java ${JAVA_ARGS} "
 
