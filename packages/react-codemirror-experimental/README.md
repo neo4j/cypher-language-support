40d4a6be15a8318517358a239575c78376d5eaf5
Oskar Damkjaer <OskarDamkjaer@users.noreply.github.com>
2023-06-13 17:06:05 +0200

    Publish private package (#47)
    * split out react-codemirror-experimental library
    
    * clean up types
    
    * move to proper lib name
    
    * prepforpublish
    
    * fix errors with 'language-support' package couldn't be found
    
    * create bundle to include our transitive dependencies
    
    * remove private
    
    * selfreview
    
    * add path to fix build
    
    * fixbuild

---
+++ packages/react-codemirror-experimental/README.md
@@ -0,0 +1,3 @@
+# React Codemirror Experimental
+
+This package can be built with `npm run build` and then published to npm with `npm publish`.
---
+++ packages/react-codemirror-experimental/rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from 'rollup-plugin-ts';
+
+import alias from '@rollup/plugin-alias';
+import { resolve } from 'path';
+import pkg from './package.json';
+
+const depsToBundle = ['language-support', 'antlr4-c3'];
+const dependenciesNotToBundle = Object.keys({
+  ...pkg.dependencies,
+  ...pkg.peerDependencies,
+})
+  .concat(['react/jsx-runtime'])
+  .filter((n) => !depsToBundle.includes(n));
+const aliases = [
+  {
+    find: 'language-support',
+    replacement: resolve(__dirname, '../language-support/src/index.ts'),
+  },
+  {
+    find: 'antlr4-c3',
+    replacement: resolve(__dirname, '../antlr4-c3/index.ts'),
+  },
+  {
+    find: '@neo4j-cypher/react-codemirror-experimental',
+    replacement: resolve(
+      __dirname,
+      '../react-codemirror-experimental/src/index.ts',
+    ),
+  },
+];
+
+export default {
+  input: 'src/index.ts',
+  external: (id) => dependenciesNotToBundle.includes(id),
+  output: [
+    { file: 'out/index.cjs', format: 'cjs' },
+    { dir: './out', format: 'es' },
+  ],
+  plugins: [typescript(), alias({ entries: aliases })],
+};
