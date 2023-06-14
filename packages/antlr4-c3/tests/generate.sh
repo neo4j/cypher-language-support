193d152ab619235fcb92b733f5d5386f232f3f27
Nacho Cordón <ncordon@users.noreply.github.com>
2023-06-14 15:15:11 +0100

    Makes VSCode tests debugging work again with several projects (#50)

--- packages/antlr4-c3/tests/generate.sh
+++ packages/antlr4-c3/tests/generate.sh
@@ -1,7 +1,6 @@
-ANTLR_JAR_FILE=/usr/local/lib/antlr4-tool.jar
 GRAMMAR_DIR=${PWD}/tests
-DOCKER_IMG=any0ne22/antlr4:4.13.0
-JAVA_ARGS="-Xmx500M -cp ${ANTLR_JAR_FILE} org.antlr.v4.Tool -Dlanguage=TypeScript"
-DOCKER_ARGS="run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v ${GRAMMAR_DIR}:/work ${DOCKER_IMG} java ${JAVA_ARGS} "
 
-docker ${DOCKER_ARGS} -no-listener -no-visitor CPP14.g4 Expr.g4 Whitebox.g4 -o /work/generated -Xexact-output-dir
+antlr4 -Dlanguage=TypeScript \
+  -no-listener -no-visitor \
+  ${GRAMMAR_DIR}/*.g4 \
+  -o ${GRAMMAR_DIR}/generated -Xexact-output-dir
