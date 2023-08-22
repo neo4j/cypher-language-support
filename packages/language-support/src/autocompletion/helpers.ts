1d08d40558ab291c04c212f312434464b5ab5d0d
Oskar Damkjær <OskarDamkjaer@users.noreply.github.com>
2023-08-22 12:30:52 +0200

    Move completions of functions, labels, reltypes and procedure names to c3 (#75)
    * refactor test helper
    
    * non-expressions done
    
    * fix incorrect caret in completions
    
    * cleanup
    
    * separate expression
    
    * remove unused code
    
    * remove unsued code
    
    * add new test case
    
    * unskip test
    
    * fix incorrect merge fixes
    
    * bring back function invocation completion code
    
    * update gitignore
    
    * all tests pass
    
    * cleanup a little
    
    * review comments
    
    * rewrite everything with nisse
    
    * fix label recommended before it's time

--- packages/language-support/src/autocompletion/helpers.ts
+++ packages/language-support/src/autocompletion/helpers.ts
@@ -1,262 +1,153 @@
-import { ParserRuleContext } from 'antlr4';
 import { CodeCompletionCore } from 'antlr4-c3';
 import {
   CompletionItem,
   CompletionItemKind,
 } from 'vscode-languageserver-types';
 import { DbInfo } from '../dbInfo';
-import CypherParser, {
-  Expression2Context,
-  LabelExpression4Context,
-  LabelExpression4IsContext,
-  NodePatternContext,
-  ProcedureNameContext,
-  RelationshipPatternContext,
-} from '../generated-parser/CypherParser';
-import { findParent, findStopNode, isDefined } from '../helpers';
+import CypherLexer from '../generated-parser/CypherLexer';
+import CypherParser from '../generated-parser/CypherParser';
 import { CypherTokenType, lexerSymbols, tokenNames } from '../lexerSymbols';
-import {
-  EnrichedParsingResult,
-  parserWrapper,
-  ParsingResult,
-} from '../parserWrapper';
+import { ParsingResult } from '../parserWrapper';
 
-export function isLabel(p: ParserRuleContext) {
-  return (
-    p instanceof LabelExpression4Context ||
-    p instanceof LabelExpression4IsContext
-  );
-}
+const labelCompletions = (dbInfo: DbInfo) =>
+  dbInfo.labels.map((labelName) => ({
+    label: labelName,
+    kind: CompletionItemKind.TypeParameter,
+  }));
 
-export function inLabel(stopNode: ParserRuleContext) {
-  const labelParent = findParent(stopNode, isLabel);
+const reltypeCompletions = (dbInfo: DbInfo) =>
+  dbInfo.relationshipTypes.map((relType) => ({
+    label: relType,
+    kind: CompletionItemKind.TypeParameter,
+  }));
+const proceduresCompletions = (dbInfo: DbInfo) =>
+  Object.keys(dbInfo.procedureSignatures).map((procedureName) => ({
+    label: procedureName,
+    kind: CompletionItemKind.Function,
+  }));
+const functionCompletions = (dbInfo: DbInfo) =>
+  Object.keys(dbInfo.functionSignatures).map((fnName) => ({
+    label: fnName,
+    kind: CompletionItemKind.Function,
+  }));
 
-  return isDefined(labelParent);
-}
-
-export function inNodeLabel(stopNode: ParserRuleContext) {
-  const labelParent = findParent(
-    findParent(stopNode, isLabel),
-    (p) => p instanceof NodePatternContext,
-  );
-
-  return isDefined(labelParent);
-}
-
-export function inRelationshipType(stopNode: ParserRuleContext) {
-  const relTypeParent = findParent(
-    findParent(stopNode, isLabel),
-    (p) => p instanceof RelationshipPatternContext,
-  );
-
-  return isDefined(relTypeParent);
-}
-
-export function parentExpression(stopNode: ParserRuleContext) {
-  return findParent(stopNode, (p) => p instanceof Expression2Context);
-}
-
-export function inProcedureName(stopNode: ParserRuleContext) {
-  const procParent = findParent(
-    stopNode,
-    (p) => p instanceof ProcedureNameContext,
-  );
-
-  return isDefined(procParent);
-}
-
-export function autocompleteLabels(dbInfo: DbInfo) {
-  return dbInfo.labels.map((t) => {
-    return {
-      label: t,
-      kind: CompletionItemKind.TypeParameter,
-    };
-  });
-}
-
-export function autocompleteRelTypes(dbInfo: DbInfo) {
-  return dbInfo.relationshipTypes.map((t) => {
-    return {
-      label: t,
-      kind: CompletionItemKind.TypeParameter,
-    };
-  });
-}
-
-export function autoCompleteFunctions(dbInfo: DbInfo, expr: ParserRuleContext) {
-  return Object.keys(dbInfo.functionSignatures)
-    .filter((functionName) => {
-      return functionName.startsWith(expr.getText());
-    })
-    .map((t) => {
-      return {
-        label: t,
-        kind: CompletionItemKind.Function,
-      };
-    });
-}
-
-export function autoCompleteProcNames(dbInfo: DbInfo) {
-  return Object.keys(dbInfo.procedureSignatures).map((t) => {
-    return {
-      label: t,
-      kind: CompletionItemKind.Function,
-    };
-  });
-}
-
-export function autoCompleteStructurally(
-  parsingResult: EnrichedParsingResult,
-  dbInfo: DbInfo,
-): CompletionItem[] | undefined {
-  const tokens = parsingResult.tokens;
-  const tree = parsingResult.result;
-  const lastTokenIndex = tokens.length - 2;
-  const lastToken = tokens[lastTokenIndex];
-  const eof = tokens[lastTokenIndex + 1];
-
-  if (lastTokenIndex <= 0) {
-    return undefined;
-    // When we have EOF with a different text in the token,
-    // it means the parser has failed to parse it.
-    // We give empty completions in that case
-    // because the query is severely broken at the
-    // point of completion (e.g. an unclosed string)
-  } else if (eof.text !== '<EOF>') {
-    return [];
-  } else if (lastToken.type === CypherParser.SPACE) {
-    // If the last token is a space, we surely cannot auto-complete using parsing tree information
-    return undefined;
-  } else {
-    const stopNode = findStopNode(tree);
-
-    if (inNodeLabel(stopNode)) {
-      return autocompleteLabels(dbInfo);
-    } else if (inRelationshipType(stopNode)) {
-      return autocompleteRelTypes(dbInfo);
-    } else {
-      // Completes expressions that are prefixes of function names as function names
-      const expr = parentExpression(stopNode);
-
-      if (isDefined(expr)) {
-        return autoCompleteFunctions(dbInfo, expr);
-      } else if (inProcedureName(stopNode)) {
-        return autoCompleteProcNames(dbInfo);
-      } else {
-        return undefined;
-      }
-    }
-  }
-}
-
-export function autoCompleteStructurallyAddingChar(
-  textUntilPosition: string,
-  dbInfo: DbInfo,
-): CompletionItem[] | undefined {
-  // Try adding a filling character, x, at the end
-  const parsingResult = parserWrapper.parse(textUntilPosition + 'x');
-  const tokens = parsingResult.tokens;
-  const tree = parsingResult.result;
-  const lastTokenIndex = tokens.length - 2;
-  const lastToken = tokens[lastTokenIndex];
-
-  if (lastTokenIndex <= 0) {
-    return undefined;
-  } else if (lastToken.type === CypherParser.SPACE) {
-    // If the last token is a space, we surely cannot auto-complete using parsing tree information
-    return undefined;
-  } else {
-    const stopNode = findStopNode(tree);
-
-    if (inNodeLabel(stopNode)) {
-      return autocompleteLabels(dbInfo);
-    } else if (inRelationshipType(stopNode)) {
-      return autocompleteRelTypes(dbInfo);
-    } else if (inLabel(stopNode)) {
-      // TODO This requires finer grain polishing
-      // Unless we build a symbol table, we cannot distinguish in a
-      // WHERE type predicate between a node:
-      //
-      // MATCH (n) WHERE n :A|B
-      // RETURN n
-      //
-      // and a relationship:
-      //
-      // MATCH (n)-[r]-(m) WHERE r :A|B
-      // RETURN n
-      return autocompleteLabels(dbInfo).concat(autocompleteRelTypes(dbInfo));
-    } else {
-      return undefined;
-    }
-  }
-}
-
-export function autoCompleteKeywords(
+export function completionCoreCompletion(
   parsingResult: ParsingResult,
+  dbInfo: DbInfo,
 ): CompletionItem[] {
   const parser = parsingResult.parser;
   const tokens = parsingResult.tokens;
 
   const codeCompletion = new CodeCompletionCore(parser);
 
-  // We always need to subtract one more for the final EOF
+  // We always need to subtract one for the EOF
   // Except if the query is empty and only contains EOF
-  const caretIndex = tokens.length > 1 ? tokens.length - 2 : 0;
+  let caretIndex = tokens.length > 1 ? tokens.length - 1 : 0;
 
-  if (caretIndex >= 0) {
-    // We need this to ignore the list of tokens from:
-    // * unescapedSymbolicNameString, because a lot of keywords are allowed there
-    // * escapedSymbolicNameString, to avoid showing ESCAPED_SYMBOLIC_NAME
-    // * stringLiteral to avoid getting autocompletions like STRING_LITERAL1, STRING_LITERAL2
-    //
-    // That way we do not populate tokens that are coming from those rules and those
-    // are collected as rule names instead
-    codeCompletion.preferredRules = new Set<number>()
-      .add(CypherParser.RULE_unescapedSymbolicNameString)
-      .add(CypherParser.RULE_escapedSymbolicNameString)
-      .add(CypherParser.RULE_stringLiteral);
+  const eof = tokens[caretIndex];
 
-    // Keep only keywords as suggestions
-    codeCompletion.ignoredTokens = new Set<number>(
-      Object.entries(lexerSymbols)
-        .filter(([, type]) => type !== CypherTokenType.keyword)
-        .map(([token]) => Number(token)),
-    );
-
-    const candidates = codeCompletion.collectCandidates(caretIndex);
-    const tokens = candidates.tokens.entries();
-
-    const tokenCandidates = Array.from(tokens).flatMap((value) => {
-      const [tokenNumber, followUpList] = value;
-
-      const firstToken = tokenNames[tokenNumber];
-      const followUpString = followUpList.indexes
-        .map((i) => tokenNames[i])
-        .join(' ');
-
-      if (firstToken === undefined) {
-        return [];
-      } else if (followUpString === '') {
-        return [firstToken];
-      } else {
-        const followUp = firstToken + ' ' + followUpString;
-        if (followUpList.optional) {
-          return [firstToken, followUp];
-        }
-
-        return [followUp];
-      }
-    });
-
-    const tokenCompletions: CompletionItem[] = tokenCandidates.map((t) => {
-      return {
-        label: t,
-        kind: CompletionItemKind.Keyword,
-      };
-    });
-
-    return tokenCompletions;
-  } else {
+  // When we have EOF with a different text in the token, it means the parser has failed to parse it.
+  // We give empty completions in that case because the query is severely broken at the
+  // point of completion (e.g. an unclosed string)
+  if (eof.text !== '<EOF>') {
     return [];
   }
+
+  // If the previous token is an identifier, we don't count it as "finished" so we move the caret back one token
+  // The identfier is finished when the last token is a SPACE or dot etc. etc.
+  // this allows us to give completions that replace the current text => for example `RET` <- it's parsed as an identifier
+  // The need for this caret movement is outlined in the documentation of antlr4-c3 in the section about caret position
+  if (tokens[caretIndex - 1]?.type === CypherLexer.IDENTIFIER) {
+    caretIndex--;
+  }
+
+  codeCompletion.preferredRules = new Set<number>([
+    CypherParser.RULE_functionName,
+    CypherParser.RULE_procedureName,
+    CypherParser.RULE_labelExpression1,
+
+    // Because of the overlap of keywords and identifiers in cypher
+    // We will suggest keywords when users type identifiers as well
+    // To avoid this we want custom completion for identifiers
+    // Until we've covered all the ways we can reach symbolic name string we'll keep this here
+    // Ideally we'd find another way to get around this issue
+    CypherParser.RULE_symbolicNameString,
+  ]);
+
+  // Keep only keywords as suggestions
+  codeCompletion.ignoredTokens = new Set<number>(
+    Object.entries(lexerSymbols)
+      .filter(([, type]) => type !== CypherTokenType.keyword)
+      .map(([token]) => Number(token)),
+  );
+
+  const candidates = codeCompletion.collectCandidates(caretIndex);
+
+  const ruleCompletions = Array.from(candidates.rules.entries()).flatMap(
+    (candidate): CompletionItem[] => {
+      const [ruleNumber, candidateRule] = candidate;
+      if (ruleNumber === CypherParser.RULE_functionName) {
+        return functionCompletions(dbInfo);
+      }
+
+      if (ruleNumber === CypherParser.RULE_procedureName) {
+        return proceduresCompletions(dbInfo);
+      }
+
+      if (ruleNumber === CypherParser.RULE_labelExpression1) {
+        const topExprIndex = candidateRule.ruleList.indexOf(
+          CypherParser.RULE_labelExpression,
+        );
+
+        const topExprParent = candidateRule.ruleList[topExprIndex - 1];
+
+        if (topExprParent === undefined) {
+          return [];
+        }
+
+        if (topExprParent === CypherParser.RULE_nodePattern) {
+          return labelCompletions(dbInfo);
+        }
+
+        if (topExprParent === CypherParser.RULE_relationshipPattern) {
+          return reltypeCompletions(dbInfo);
+        }
+
+        if (topExprParent === CypherParser.RULE_labelExpressionPredicate) {
+          return [...labelCompletions(dbInfo), ...reltypeCompletions(dbInfo)];
+        }
+      }
+      return [];
+    },
+  );
+
+  const tokenEntries = candidates.tokens.entries();
+  const tokenCandidates = Array.from(tokenEntries).flatMap((value) => {
+    const [tokenNumber, followUpList] = value;
+
+    const firstToken = tokenNames[tokenNumber];
+    const followUpString = followUpList.indexes
+      .map((i) => tokenNames[i])
+      .join(' ');
+
+    if (firstToken === undefined) {
+      return [];
+    } else if (followUpString === '') {
+      return [firstToken];
+    } else {
+      const followUp = firstToken + ' ' + followUpString;
+      if (followUpList.optional) {
+        return [firstToken, followUp];
+      }
+
+      return [followUp];
+    }
+  });
+
+  const tokenCompletions: CompletionItem[] = tokenCandidates.map((t) => ({
+    label: t,
+    kind: CompletionItemKind.Keyword,
+  }));
+
+  return [...ruleCompletions, ...tokenCompletions];
 }
--- packages/language-support/src/tests/testHelpers.ts
+++ packages/language-support/src/tests/testHelpers.ts
@@ -2,20 +2,11 @@ import { SignatureInformation } from 'vscode-languageserver-types';
 import { DbInfo } from '../dbInfo';
 
 export class MockDbInfo implements DbInfo {
-  public procedureSignatures: Record<string, SignatureInformation>;
-  public functionSignatures: Record<string, SignatureInformation>;
-  public labels: string[];
-  public relationshipTypes: string[];
-
   constructor(
-    labels: string[] = [],
-    relationshipTypes: string[] = [],
-    procedureSignatures: Record<string, SignatureInformation> = {},
-    functionSignatures: Record<string, SignatureInformation> = {},
-  ) {
-    this.labels = labels;
-    this.relationshipTypes = relationshipTypes;
-    this.procedureSignatures = procedureSignatures;
-    this.functionSignatures = functionSignatures;
-  }
+    public labels: string[] = [],
+    public relationshipTypes: string[] = [],
+    public procedureSignatures: Record<string, SignatureInformation> = {},
+    public functionSignatures: Record<string, SignatureInformation> = {},
+    public databaseNames: string[] = [],
+  ) {}
 }
