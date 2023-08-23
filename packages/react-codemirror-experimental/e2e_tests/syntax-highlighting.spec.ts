53bedd4e9ad92653161d5b32141808cff703117d
Oskar Damkjær <OskarDamkjaer@users.noreply.github.com>
2023-08-23 10:38:45 +0200

    fix multiline highlighting for codemirror (#76)
    * fix multiline highlighting for codemirror
    
    * mellan
    
    * write tests to test startoffset
    
    * add tests for multiline label and comment
    
    * fix test that was failing after mergin main

--- packages/react-codemirror-experimental/e2e_tests/syntax-highlighting.spec.ts
+++ packages/react-codemirror-experimental/e2e_tests/syntax-highlighting.spec.ts
@@ -119,3 +119,46 @@ RETURN variable;`;
     ),
   ).toEqual(darkThemeConstants.highlightStyles.numberLiteral);
 });
+
+test('highlights multiline string literal correctly', async ({ page }) => {
+  const editorPage = new CypherEditorPage(page);
+  const query = `
+RETURN "
+multilinestring";`;
+
+  await editorPage.createEditor({ value: query, theme: 'light' });
+
+  expect(
+    await editorPage.getHexColorOfLocator(page.getByText('multilinestring')),
+  ).toEqual(lightThemeConstants.highlightStyles.stringLiteral);
+});
+
+test('highlights multiline label correctly', async ({ page }) => {
+  const editorPage = new CypherEditorPage(page);
+  const query = `
+MATCH (v:\`
+
+Label\`)
+`;
+
+  await editorPage.createEditor({ value: query, theme: 'light' });
+
+  expect(
+    await editorPage.getHexColorOfLocator(page.getByText('Label')),
+  ).toEqual(lightThemeConstants.highlightStyles.label);
+});
+
+test('highlights multiline comment correctly', async ({ page }) => {
+  const editorPage = new CypherEditorPage(page);
+  const query = `
+/*
+
+comment
+*/";`;
+
+  await editorPage.createEditor({ value: query, theme: 'light' });
+
+  expect(
+    await editorPage.getHexColorOfLocator(page.getByText('comment')),
+  ).toEqual(lightThemeConstants.highlightStyles.comment);
+});
