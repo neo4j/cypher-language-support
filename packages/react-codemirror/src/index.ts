297c9191d5fb6266d53a42568c83e870bcfd578e
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2023-10-25 16:08:42 +0200

    remove uiw

--- packages/react-codemirror/src/index.ts
+++ packages/react-codemirror/src/index.ts
@@ -1,5 +1,4 @@
 export { CypherParser, parse } from '@neo4j-cypher/language-support';
-export * as ReactCodemirror from '@uiw/react-codemirror';
 export { CypherEditor } from './CypherEditor';
 export { cypher } from './lang-cypher/lang-cypher';
 export { darkThemeConstants, lightThemeConstants } from './themes';
