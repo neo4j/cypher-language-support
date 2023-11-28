43fcaf43fb0ff64fe5c97462c10d53aa35b42bbc
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2023-11-28 16:59:56 +0100

    add benchmarks for react-wrapper

--- packages/react-codemirror/src/e2e_tests/performance-test.spec.tsx
+++ packages/react-codemirror/src/e2e_tests/performance-test.spec.tsx
@@ -1,12 +1,42 @@
 import { expect, test } from '@playwright/experimental-ct-react';
+import fs from 'fs';
 import { CypherEditor } from '../CypherEditor';
 import { CypherEditorPage } from './e2e-utils';
 import { largeQuery, mockSchema } from './mock-data';
 
 test.use({ viewport: { width: 1000, height: 500 } });
+declare global {
+  interface Window {
+    longtasks: number[];
+  }
+}
 
-test('performance test session ', async ({ mount, page }) => {
-  test.setTimeout(30 * 1000);
+test('benchmarking & performance test session', async ({
+  mount,
+  page,
+  browser,
+}) => {
+  if (process.env.BENCHMARKING === 'true') {
+    test.setTimeout(1000000);
+    const client = await page.context().newCDPSession(page);
+    await client.send('Performance.enable');
+    await client.send('Emulation.setCPUThrottlingRate', { rate: 4 });
+    await client.send('Overlay.setShowFPSCounter', { show: true });
+    await browser.startTracing(page, {
+      path: './trace.json',
+      screenshots: true,
+    });
+    await page.evaluate(() => {
+      window.longtasks = [];
+      const observer = new PerformanceObserver((list) => {
+        window.longtasks.push(...list.getEntries().map((e) => e.duration));
+      });
+
+      observer.observe({ entryTypes: ['longtask'] });
+    });
+  } else {
+    test.setTimeout(30 * 1000);
+  }
   const editorPage = new CypherEditorPage(page);
   const component = await mount(
     <CypherEditor prompt="neo4j>" theme="dark" lint schema={mockSchema} />,
@@ -68,4 +98,35 @@ test('performance test session ', async ({ mount, page }) => {
   await editorPage
     .getEditor()
     .pressSequentially('veryveryveryverylongvariable');
+
+  if (process.env.BENCHMARKING === 'true') {
+    // remove value and schema to see memory usage lowering again while idle
+    await component.update(<CypherEditor value={''} schema={{}} />);
+    await page.evaluate(() => window.performance.mark('start idling'));
+    // Used to see memeory usage over time while idle
+    await page.waitForTimeout(2 * 60 * 1000);
+    await browser.stopTracing();
+
+    // save long task information to json
+    const longtasks = await page.evaluate(() => window.longtasks);
+    const sortedLongTasks = longtasks.sort((a, b) => a - b);
+    const medianLongTask =
+      sortedLongTasks[Math.floor(sortedLongTasks.length / 2)];
+    const averageLongTask =
+      sortedLongTasks.reduce((a, b) => a + b, 0) / sortedLongTasks.length;
+    const over500 = sortedLongTasks.filter((t) => t > 500).length;
+    const nintyninethPercentile =
+      sortedLongTasks[Math.floor(sortedLongTasks.length * 0.99)];
+
+    fs.writeFileSync(
+      'stats.json',
+      JSON.stringify({
+        longtasksCount: longtasks.length,
+        medianLongTask,
+        averageLongTask,
+        over500,
+        nintyninethPercentile,
+      }),
+    );
+  }
 });
