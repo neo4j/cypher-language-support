373889e16500668b58741690f0ad29d07708a6be
Nacho Cordón <nacho.cordon@neotechnology.com>
2024-01-23 10:23:24 +0000

    Reshapes signature help unit tests to use mock data

--- packages/language-support/src/tests/signatureHelp.test.ts
+++ packages/language-support/src/tests/signatureHelp.test.ts
@@ -5,6 +5,7 @@ import {
 } from 'vscode-languageserver-types';
 import { DbSchema } from '../dbSchema';
 import { emptyResult, signatureHelp } from '../signatureHelp';
+import { testData } from './testData';
 
 export function testSignatureHelp(
   fileText: string,
@@ -20,21 +21,15 @@ export function testSignatureHelp(
 }
 
 describe('Procedures signature help', () => {
+  const dbSchema = testData.mockSchema;
+  const procedureName = 'apoc.do.when';
   const signature = SignatureInformation.create(
-    'apoc.do.when',
-    'Runs the given read/write ifQuery if the conditional has evaluated to true, otherwise the elseQuery will run.',
-    ...[
-      ParameterInformation.create('condition', 'condition :: BOOLEAN?'),
-      ParameterInformation.create('ifQuery', 'ifQuery :: STRING?'),
-      ParameterInformation.create('elseQuery', 'condition :: STRING?'),
-      ParameterInformation.create('params', 'params = {} :: MAP?'),
-    ],
+    procedureName,
+    dbSchema.procedureSignatures[procedureName].documentation,
+    ...dbSchema.procedureSignatures[procedureName].parameters.map((param) =>
+      ParameterInformation.create(param.label, param.documentation),
+    ),
   );
-  const dbWithProcedure: DbSchema = {
-    procedureSignatures: {
-      'apoc.do.when': signature,
-    },
-  };
 
   function expectedArgIndex(i: number): SignatureHelp {
     return {
@@ -48,42 +43,30 @@ describe('Procedures signature help', () => {
     testSignatureHelp(
       `MATCH (n)
          CALL apoc.do.when(`,
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(0),
     );
 
     testSignatureHelp(
       `MATCH (n)
          CALL apoc.do.when(true`,
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(0),
     );
   });
 
   test('Provides signature help for CALLs first argument', () => {
-    testSignatureHelp(
-      'CALL apoc.do.when(',
-      dbWithProcedure,
-      expectedArgIndex(0),
-    );
+    testSignatureHelp('CALL apoc.do.when(', dbSchema, expectedArgIndex(0));
 
-    testSignatureHelp(
-      'CALL apoc.do.when(true',
-      dbWithProcedure,
-      expectedArgIndex(0),
-    );
+    testSignatureHelp('CALL apoc.do.when(true', dbSchema, expectedArgIndex(0));
   });
 
   test('Provides signature help for CALLs second argument', () => {
-    testSignatureHelp(
-      'CALL apoc.do.when(true,',
-      dbWithProcedure,
-      expectedArgIndex(1),
-    );
+    testSignatureHelp('CALL apoc.do.when(true,', dbSchema, expectedArgIndex(1));
 
     testSignatureHelp(
       'CALL apoc.do.when(true, "foo"',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(1),
     );
   });
@@ -91,13 +74,13 @@ describe('Procedures signature help', () => {
   test('Provides signature help for CALLs third argument', () => {
     testSignatureHelp(
       'CALL apoc.do.when(true, "foo",',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(2),
     );
 
     testSignatureHelp(
       'CALL apoc.do.when(true, "foo", false',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(2),
     );
   });
@@ -105,13 +88,13 @@ describe('Procedures signature help', () => {
   test('Provides signature help for CALLs fourth argument', () => {
     testSignatureHelp(
       'CALL apoc.do.when(true, "foo", false,',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(3),
     );
 
     testSignatureHelp(
       'CALL apoc.do.when(true, "foo", false, "bar"',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(3),
     );
   });
@@ -120,7 +103,7 @@ describe('Procedures signature help', () => {
     testSignatureHelp(
       `MATCH (n) RETURN n;
          CALL apoc.do.when(`,
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(0),
     );
   });
@@ -130,26 +113,26 @@ describe('Procedures signature help', () => {
       `CALL apoc.do.when(true, "foo", false, "bar");
    
          MATCH (`,
-      dbWithProcedure,
+      dbSchema,
       emptyResult,
     );
   });
 
   test('Does not provide blow up for caret position of 0', () => {
-    testSignatureHelp('', dbWithProcedure, emptyResult);
+    testSignatureHelp('', dbSchema, emptyResult);
   });
 
   test('Provides signature help for CALLs when offset is before end of query for first argument', () => {
     testSignatureHelp(
       'CALL apoc.do.when(true, something',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(0),
       18,
     );
 
     testSignatureHelp(
       'CALL apoc.do.when(true, something',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(0),
       22,
     );
@@ -158,14 +141,14 @@ describe('Procedures signature help', () => {
   test('Provides signature help for CALLs when offset is before end of query for second argument', () => {
     testSignatureHelp(
       'CALL apoc.do.when(true, foo, bar',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(1),
       23,
     );
 
     testSignatureHelp(
       'CALL apoc.do.when(true, foo, bar',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(1),
       27,
     );
@@ -174,7 +157,7 @@ describe('Procedures signature help', () => {
   test('Provides signature help for CALLs even on several spaces', () => {
     testSignatureHelp(
       'CALL apoc.do.when(true,   )',
-      dbWithProcedure,
+      dbSchema,
       expectedArgIndex(1),
       25,
     );
@@ -182,20 +165,15 @@ describe('Procedures signature help', () => {
 });
 
 describe('Functions signature help', () => {
+  const dbSchema = testData.mockSchema;
+  const functionName = 'apoc.coll.combinations';
   const signature = SignatureInformation.create(
-    'apoc.do.when',
-    'Runs the given read/write ifQuery if the conditional has evaluated to true, otherwise the elseQuery will run.',
-    ...[
-      ParameterInformation.create('condition', 'condition :: BOOLEAN?'),
-      ParameterInformation.create('ifQuery', 'ifQuery :: STRING?'),
-      ParameterInformation.create('elseQuery', 'condition :: STRING?'),
-    ],
+    functionName,
+    dbSchema.functionSignatures[functionName].documentation,
+    ...dbSchema.functionSignatures[functionName].parameters.map((param) =>
+      ParameterInformation.create(param.label, param.documentation),
+    ),
   );
-  const dbWithFunction: DbSchema = {
-    functionSignatures: {
-      'apoc.do.when': signature,
-    },
-  };
 
   function expectedArgIndex(i: number): SignatureHelp {
     return {
@@ -208,8 +186,8 @@ describe('Functions signature help', () => {
   test('Provides signature help for functions first argument when argument non started', () => {
     testSignatureHelp(
       `MATCH (n)
-         RETURN apoc.do.when(`,
-      dbWithFunction,
+         RETURN apoc.coll.combinations(`,
+      dbSchema,
       expectedArgIndex(0),
     );
   });
@@ -217,36 +195,36 @@ describe('Functions signature help', () => {
   test('Provides signature help for functions first argument when argument started', () => {
     testSignatureHelp(
       `MATCH (n)
-         RETURN apoc.do.when(true`,
-      dbWithFunction,
+         RETURN apoc.coll.combinations(coll`,
+      dbSchema,
       expectedArgIndex(0),
     );
   });
 
   test('Provides signature help for functions second argument', () => {
     testSignatureHelp(
-      'MATCH (n) WHERE apoc.do.when(true,',
-      dbWithFunction,
+      'MATCH (n) WHERE apoc.coll.combinations(coll,',
+      dbSchema,
       expectedArgIndex(1),
     );
 
     testSignatureHelp(
-      'MATCH (n) WHERE apoc.do.when(true, "foo"',
-      dbWithFunction,
+      'MATCH (n) WHERE apoc.coll.combinations(coll, "foo"',
+      dbSchema,
       expectedArgIndex(1),
     );
   });
 
   test('Provides signature help for functions third argument', () => {
     testSignatureHelp(
-      'RETURN true OR apoc.do.when(true, "foo",',
-      dbWithFunction,
+      'RETURN true OR apoc.coll.combinations(coll, "foo",',
+      dbSchema,
       expectedArgIndex(2),
     );
 
     testSignatureHelp(
-      'RETURN true OR apoc.do.when(true, "foo", false',
-      dbWithFunction,
+      'RETURN true OR apoc.coll.combinations(coll, "foo", false',
+      dbSchema,
       expectedArgIndex(2),
     );
   });
@@ -254,55 +232,55 @@ describe('Functions signature help', () => {
   test('Provides signature help with several statements where cursor one requires autocompletion', () => {
     testSignatureHelp(
       `MATCH (n) RETURN n;
-         MATCH (m) WHERE apoc.do.when(t`,
-      dbWithFunction,
+         MATCH (m) WHERE apoc.coll.combinations(c`,
+      dbSchema,
       expectedArgIndex(0),
     );
   });
 
   test('Does not provide signature help with several statements where cursor one does not require autocompletion', () => {
     testSignatureHelp(
-      `RETURN apoc.do.when(true, "foo", false, "bar");
+      `RETURN apoc.coll.combinations(coll, minSelect, maxSelect);
    
          MATCH (`,
-      dbWithFunction,
+      dbSchema,
       emptyResult,
     );
   });
 
   test('Does not provide blow up for caret position of 0', () => {
-    testSignatureHelp('', dbWithFunction, emptyResult);
+    testSignatureHelp('', dbSchema, emptyResult);
   });
 
   test('Provides signature help for functions when offset is before end of query for first argument', () => {
     testSignatureHelp(
-      'RETURN apoc.do.when(true, something',
-      dbWithFunction,
+      'MATCH (n) WHERE apoc.coll.combinations(coll, "foo"',
+      dbSchema,
       expectedArgIndex(0),
-      20,
+      39,
     );
 
     testSignatureHelp(
-      'RETURN apoc.do.when(true, something',
-      dbWithFunction,
+      'MATCH (n) WHERE apoc.coll.combinations(coll, "foo"',
+      dbSchema,
       expectedArgIndex(0),
-      24,
+      43,
     );
   });
 
   test('Provides signature help for functions when offset is before end of query for second argument', () => {
     testSignatureHelp(
-      'RETURN apoc.do.when(true, foo, bar',
-      dbWithFunction,
+      'MATCH (n) WHERE apoc.coll.combinations(coll, "foo", bar',
+      dbSchema,
       expectedArgIndex(1),
-      25,
+      45,
     );
 
     testSignatureHelp(
-      'RETURN apoc.do.when(true, foo, bar',
-      dbWithFunction,
+      'MATCH (n) WHERE apoc.coll.combinations(coll, "foo", bar',
+      dbSchema,
       expectedArgIndex(1),
-      29,
+      50,
     );
   });
 });
