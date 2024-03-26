8c952ebfe3f1353d0b5e9464cefb5ca4aec3f6c6
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2024-03-26 09:38:33 +0100

    add test

--- packages/language-support/src/autocompletion/completionCoreCompletions.ts
+++ packages/language-support/src/autocompletion/completionCoreCompletions.ts
@@ -283,13 +283,6 @@ function calculateNamespacePrefix(
   return namespacePrefix;
 }
 
-const baseTriggerCharacters = ['.', '(', ')', ','];
-export const snippetTriggerCharacters = ['<', '-'];
-export const completionTriggerCharacters = [
-  ...baseTriggerCharacters,
-  ...snippetTriggerCharacters,
-];
-
 export function completionCoreCompletion(
   parsingResult: ParsedStatement,
   dbSchema: DbSchema,
