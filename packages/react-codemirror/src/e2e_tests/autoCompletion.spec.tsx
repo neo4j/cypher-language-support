2c2aea02e62e7fee6a1bb0b609b603315199350f
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2024-04-02 16:16:41 +0200

    update update key map for completions in snippets

--- packages/react-codemirror/src/e2e_tests/autoCompletion.spec.tsx
+++ packages/react-codemirror/src/e2e_tests/autoCompletion.spec.tsx
@@ -246,9 +246,38 @@ test('can complete pattern snippet', async ({ page, mount }) => {
 
   await textField.press('Tab');
   await textField.press('Tab');
-  await textField.press('Tab');
 
   await expect(textField).toHaveText('MATCH ()-[]->()-[ ]->( )');
+});
+
+test('can accept completion inside pattern snippet', async ({
+  page,
+  mount,
+}) => {
+  await mount(<CypherEditor schema={{ labels: ['City'] }} />);
+  const textField = page.getByRole('textbox');
+
+  await textField.fill('MATCH ()-[]->()');
+
+  await page.locator('.cm-tooltip-autocomplete').getByText('-[]->()').click();
+  await expect(page.locator('.cm-tooltip-autocomplete')).not.toBeVisible();
+
+  // move to node
+  await textField.press('Tab');
+
+  // get & accept completion
+  await textField.press(':');
+  await expect(
+    page.locator('.cm-tooltip-autocomplete').getByText('City'),
+  ).toBeVisible();
+
+  await textField.press('Tab');
+  await expect(page.locator('.cm-tooltip-autocomplete')).not.toBeVisible();
+
+  // tab out of the snippet
+  await textField.press('Tab');
+
+  await expect(textField).toHaveText('MATCH ()-[]->()-[ ]->(:City)');
 });
 
 test('does not automatically open completion panel for expressions after snippet trigger char', async ({
