dbe560fa9c3272e7935de6f15575613896241c65
Nacho Cordón <ncordon@users.noreply.github.com>
2024-05-21 12:46:48 +0100

    Wires feature flags (#211)

--- packages/react-codemirror/src/index.ts
+++ packages/react-codemirror/src/index.ts
@@ -1,4 +1,7 @@
-export { CypherParser } from '@neo4j-cypher/language-support';
+export {
+  CypherParser,
+  _internalFeatureFlags,
+} from '@neo4j-cypher/language-support';
 export { CypherEditor } from './CypherEditor';
 export { cypher } from './lang-cypher/langCypher';
 export { darkThemeConstants, lightThemeConstants } from './themes';
