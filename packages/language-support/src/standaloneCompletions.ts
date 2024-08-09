575484ce5de3a8d245c3403aa79cdc7fabc0ecc9
Nacho Cordón <nacho.cordon@neotechnology.com>
2024-08-09 10:40:59 +0200

    Reshapes schema

--- packages/language-support/src/standaloneCompletions.ts
+++ packages/language-support/src/standaloneCompletions.ts
@@ -1,3 +1,27 @@
 import { autocomplete } from './autocompletion/autocompletion';
+import { Neo4jFunction, Neo4jProcedure } from './types';
 
-globalThis.autocomplete = autocomplete;
+export interface DbSchemaNative {
+  labels?: string[];
+  relationshipTypes?: string[];
+  databaseNames?: string[];
+  aliasNames?: string[];
+  userNames?: string[];
+  roleNames?: string[];
+  parameters?: Map<string, unknown>;
+  propertyKeys?: string[];
+  procedures?: Map<string, Neo4jProcedure>;
+  functions?: Map<string, Neo4jFunction>;
+}
+
+function standaloneAutocomplete(query: string, dbSchemaNative: DbSchemaNative) {
+  const dbSchema = {
+    ...dbSchemaNative,
+    procedures: Object.fromEntries(dbSchemaNative.procedures),
+    functions: Object.fromEntries(dbSchemaNative.functions),
+    parameters: Object.fromEntries(dbSchemaNative.parameters),
+  };
+  return autocomplete(query, dbSchema);
+}
+
+globalThis.autocomplete = standaloneAutocomplete;
