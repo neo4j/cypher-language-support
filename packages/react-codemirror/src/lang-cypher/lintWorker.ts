d363872ea0f46d0ac464723f9449425e1df6985d
Nacho Cordon <nacho.cordon@neotechnology.com>
2024-12-03 17:59:20 +0000

    Make the codemirror project work with the new syntax errors

--- packages/react-codemirror/src/lang-cypher/lintWorker.ts
+++ packages/react-codemirror/src/lang-cypher/lintWorker.ts
@@ -8,5 +8,5 @@ type LinterArgs = Parameters<typeof lintCypherQuery>;
 export type LinterTask = workerpool.Promise<ReturnType<typeof lintCypherQuery>>;
 
 export type LintWorker = {
-  validateSemantics: (...args: LinterArgs) => LinterTask;
+  lintCypherQuery: (...args: LinterArgs) => LinterTask;
 };
--- packages/react-codemirror/src/lang-cypher/syntaxValidation.ts
+++ packages/react-codemirror/src/lang-cypher/syntaxValidation.ts
@@ -33,10 +33,7 @@ export const semanticAnalysisLinter: (config: CypherConfig) => Extension = (
       }
 
       const proxyWorker = (await pool.proxy()) as unknown as LintWorker;
-      lastSemanticJob = proxyWorker.validateSemantics(
-        query,
-        config.schema ?? {},
-      );
+      lastSemanticJob = proxyWorker.lintCypherQuery(query, config.schema ?? {});
       const result = await lastSemanticJob;
 
       return result.map((diag) => {
