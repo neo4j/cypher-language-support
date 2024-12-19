78405cbdbb833a0b91a798fffdb06091ae83ab62
Isak <isak.nilsson@neo4j.com>
2024-12-19 17:16:12 +0100

    added testing of cypher 5 completion

--- packages/language-support/src/tests/autocompletion/miscCompletions.test.ts
+++ packages/language-support/src/tests/autocompletion/miscCompletions.test.ts
@@ -5,6 +5,24 @@ import {
 } from './completionAssertionHelpers';
 
 describe('Misc auto-completion', () => {
+  test('Correctly completes cypher version number', () => {
+    const query = 'CYPHER ';
+
+    testCompletions({
+      query,
+      expected: [{ label: '5', kind: CompletionItemKind.EnumMember }],
+    });
+  });
+
+  test('Correctly completes cypher keyword and version together', () => {
+    const query = 'CYP';
+
+    testCompletions({
+      query,
+      expected: [{ label: 'CYPHER 5', kind: CompletionItemKind.Keyword }],
+    });
+  });
+
   test('Correctly completes empty statement', () => {
     const query = '';
 
