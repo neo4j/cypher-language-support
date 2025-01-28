e6c090cc980b3e939f7ee2251a37a3a2468f74ae
Tomas Nyberg <71550631+tomasnyberg@users.noreply.github.com>
2025-01-28 15:27:53 +0100

    Adds standardization check to formatter tests (#334)

--- packages/language-support/src/formatting/formatting.ts
+++ packages/language-support/src/formatting/formatting.ts
@@ -1,14 +1,6 @@
+import { CommonTokenStream, ParserRuleContext, TerminalNode } from 'antlr4';
+import { default as CypherCmdLexer } from '../generated-parser/CypherCmdLexer';
 import {
-  CharStreams,
-  CommonTokenStream,
-  ParserRuleContext,
-  TerminalNode,
-} from 'antlr4';
-import {
-  default as CypherCmdLexer,
-  default as CypherLexer,
-} from '../generated-parser/CypherCmdLexer';
-import CypherCmdParser, {
   ArrowLineContext,
   BooleanLiteralContext,
   ClauseContext,
@@ -31,6 +23,8 @@ import CypherCmdParser, {
 import CypherCmdParserVisitor from '../generated-parser/CypherCmdParserVisitor';
 import {
   findTargetToken,
+  getParseTreeAndTokens,
+  handleMergeClause,
   isComment,
   wantsSpaceAfter,
   wantsSpaceBefore,
@@ -326,23 +320,7 @@ export class TreePrintVisitor extends CypherCmdParserVisitor<void> {
 
   // Handled separately because we want ON CREATE before ON MATCH
   visitMergeClause = (ctx: MergeClauseContext) => {
-    this.visit(ctx.MERGE());
-    this.visit(ctx.pattern());
-    const mergeActions = ctx
-      .mergeAction_list()
-      .map((action, index) => ({ action, index }))
-      .sort((a, b) => {
-        if (a.action.CREATE() && b.action.MATCH()) {
-          return -1;
-        } else if (a.action.MATCH() && b.action.CREATE()) {
-          return 1;
-        }
-        return a.index - b.index;
-      })
-      .map(({ action }) => action);
-    mergeActions.forEach((action) => {
-      this.visit(action);
-    });
+    handleMergeClause(ctx, (node) => this.visit(node));
   };
 
   // Handled separately because it wants indentation
@@ -393,13 +371,9 @@ export function formatQuery(
   query: string,
   cursorPosition?: number,
 ): string | FormattingResultWithCursor {
-  const inputStream = CharStreams.fromString(query);
-  const lexer = new CypherLexer(inputStream);
-  const tokens = new CommonTokenStream(lexer);
+  const { tree, tokens } = getParseTreeAndTokens(query);
   tokens.fill();
-  const parser = new CypherCmdParser(tokens);
-  parser.buildParseTrees = true;
-  const tree = parser.statementsOrCommands();
+
   const visitor = new TreePrintVisitor(tokens);
 
   if (cursorPosition === undefined) return visitor.format(tree);
