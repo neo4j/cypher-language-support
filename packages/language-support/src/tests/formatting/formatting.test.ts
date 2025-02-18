160aaa54d6433fbeae941aeb19877d0f1bbbde29
simonthuresson <sthuresson00@gmail.com>
2025-02-18 15:50:04 +0100

    fixes so nested FOREACH gets newline and indent

--- packages/language-support/src/tests/formatting/formatting.test.ts
+++ packages/language-support/src/tests/formatting/formatting.test.ts
@@ -447,6 +447,28 @@ WHERE (n[listItem] IS FLOAT OR n[listItem] IS INTEGER)
 RETURN n`;
     verifyFormatting(query, expected);
   });
+  test('should put nested FOREACH on newline', () => {
+    const query = `MATCH (u:User)
+MATCH (u)-[:USER_EVENT]->(e:Event)
+WITH u, e ORDER BY e ASC
+WITH u, collect(e) AS eventChain
+FOREACH (i IN range(0, size(eventChain) - 2) |
+FOREACH (node1 IN [eventChain [i]] |
+FOREACH (node2 IN [eventChain [i + 1]] |
+MERGE (node1)-[:NEXT_EVENT]->(node2))))`;
+    const expected = `MATCH (u:User)
+MATCH (u)-[:USER_EVENT]->(e:Event)
+WITH u, e ORDER BY e ASC
+WITH u, collect(e) AS eventChain
+FOREACH (i IN range(0, size(eventChain) - 2) |
+  FOREACH (node1 IN [eventChain[i]] |
+    FOREACH (node2 IN [eventChain[i + 1]] |
+      MERGE (node1)-[:NEXT_EVENT]->(node2)
+    )
+  )
+)`;
+    verifyFormatting(query, expected);
+  });
 });
 
 describe('tests for correct cursor position', () => {
