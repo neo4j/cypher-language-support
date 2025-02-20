394a522233720dbdc843766d93753c3d017232a1
simonthuresson <sthuresson00@gmail.com>
2025-02-20 17:46:52 +0100

    adds test for call clause

--- packages/language-support/src/tests/formatting/formattingv2.test.ts
+++ packages/language-support/src/tests/formatting/formattingv2.test.ts
@@ -647,6 +647,7 @@ RETURN variable;`;
   const q21 = `MATCH path = (m1:loooooooongrelationtypename {code: "mFG66X9v"})-[
 r:verylongrelationtypename]->(m2:anotherverylongrelationtypename)
 RETURN path`;
+const q24 = `CALL apoc.periodic.iterate ("eZ0sadadawdawdsdsdsdq", "1p7sdsdsasdwasddsdEsdsd", {baisdsdadadze: "v0Asdsdsdadadadsdsdp", paladadadel: "UsdssdsdsddUg"})`;
 
   const queries = [
     q0,
@@ -671,6 +672,7 @@ RETURN path`;
     q19,
     q20,
     q21,
+    q24
   ];
 
   test('keeps all queries within the max column width', () => {
@@ -868,6 +870,20 @@ RETURN p`;
     verifyFormatting(query, expected);
   });
 
+  test('should not leave dangling closing brace/parenthesis for this query', () => {
+    const query = `EXPLAIN MATCH (rye { title: "HJrZJBJC" })
+CREATE (n:Tyyyyyyyyype { title: "Q8Ha5pVR", rank: "DIVe5wjf", rankID: "9ukDjNtj" })
+CREATE ()
+RETURN rye`;
+    const expected = `EXPLAIN
+MATCH (rye {title: "HJrZJBJC"})
+CREATE (n:Tyyyyyyyyype {title: "Q8Ha5pVR", rank: "DIVe5wjf",
+                       rankID: "9ukDjNtj"})
+CREATE ()
+RETURN rye`;
+    verifyFormatting(query, expected);
+  });
+
   test('should not forget about alignment for unwind clause', () => {
     const query = `UNWIND [{_id:"MiltPFxk", properties:{name:"5nIou0gC", id:"ha44MrBy", value:"6o5lzHd6"}}, {_id:"2uMA2cW8", properties:{name:"WOsBC4Ks", id:"bP526OzE", value:"WhYP4dxd"}}] AS row RETURN row`;
     const expected = `
@@ -908,6 +924,14 @@ MERGE (naame)-[:tyyyyyyyyyype {keeeeeeeey: "dFTkCNlb", keey: "rmmCQGIb"}]->
     verifyFormatting(query, expected);
   });
 
+  test('tests call case', () => {
+    const query = `CALL apoc.periodic.iterate("eZQB0P0q", "1p7EFkyE", {batchSize: "v0Ap5F8F", parallel: "UUc75lVg"}) YIELD batches, total, timeTaken, committedOperations, failedOperations`;
+    const expected = `CALL apoc.periodic.iterate("eZQB0P0q", "1p7EFkyE",
+                           {batchSize: "v0Ap5F8F", parallel: "UUc75lVg"})
+YIELD batches, total, timeTaken, committedOperations, failedOperations`.trimStart();
+    verifyFormatting(query, expected);
+  });
+
   test('does not split weird parenthesized expressions weirdly', () => {
     const query = `MATCH (p:Product)--(o:Order)
 WHERE (p.priiiiiiiiiiiiiiiiiiice + o.siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiize)
@@ -916,6 +940,73 @@ RETURN p;`;
 WHERE (p.priiiiiiiiiiiiiiiiiiice +
        o.siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiize)
 RETURN p;`;
+    verifyFormatting(query, expected);
+  });
+});
+
+describe('tests for line breaks with comments', () => {
+  test('should not put the arrow on a newline for no reason', () => {
+    const query = `match (z:Consumer {zen_id: "T1M3wiuA"})-[:HAS_INTERACTION]-(i)
+MATCH (i:Interaction:PageView)-[:HAS_TARGET]->(t)
+//MATCH (t:Target)-[:BELONGS_TO]->(s)
+return z`;
+    const expected = `MATCH (z:Consumer {zen_id: "T1M3wiuA"})-[:HAS_INTERACTION]-(i)
+MATCH (i: Interaction:PageView)-[:HAS_TARGET]->(t)
+//MATCH (t:Target)-[:BELONGS_TO]->(s)
+RETURN z`;
+    verifyFormatting(query, expected);
+  });
+
+  test('handles comments before long patterns gracefully', () => {
+    const query = `CREATE
+// this is a loooooooooooooooooooong comment
+(qwer_tyuiopa_zxcvbnmasdfg)-[:abcdefgh]->(qwertyu),
+(qwertyu)-[:HIJKLMN_OP]->(asdfghj_klzxcvbnmop),
+(asdfghj_klzxcvbnmop)-[:QRSTUVWX]->(qazwsxedc_rfvgt),
+(mnbvcxzasdfghj_poiuytrewq)-[:YZABCDF]->(qwertyu),
+(mnbvcxzasdfghj_poiuytrewq)-[:GHIJKLMN]->(zxcvbnmlkjhgfd_asdfjkl),
+(zxcvbnmlkjhgfd_asdfjkl)-[:OPQRS_TU]->(qwertyu),
+(qwert_yuiopasdfg)-[:OPQRS_TU]->(qwertyu),
+
+// this is a loooooooooooooooooooong comment
+(hjklmno)-[:OPQRS_TU]->(zxcvbn_mnb_lkjhgfdsa),
+(zxcvbn_mnb_lkjhgfdsa)-[:OPQRS_TU]->(poiuzxcv),
+(poiuzxcv)-[:OPQRS_TU]->(asdfghjk_qwe),
+(asdfghjk_qwe)-[:OPQRS_TU]->(zxcvbnmop),
+(zxcvbnmop)-[:OPQRS_TU]->(qwertyu),
+(zxcvbnmop)-[:VWXYZABC]->(qwertyuiopa_sdfghjklz),
+
+// this is a loooooooooooooooooooong comment
+(mnbvcxzlkj)-[:VWXYZABC]->(asdfg_hjkltyui),
+(mnbvcxzlkj)-[:VWXYZABC]->(qwertyuiopa_sdfghjklz),
+
+// this is a loooooooooooooooooooong comment
+(mnbvcxzasdfghj_poiuytrewq)-[:YZABCDF]->(asdfghj_klzxcvbnmop),
+(mnbvcxzasdfghj_poiuytrewq)-[:DEFHIJKL]->(qazwsxedc_rfvgt),
+(mnbvcxzasdfghj_poiuytrewq)-[:MNOPQRST]->(qwert_yuiopasdfg);`;
+    const expected = `
+CREATE // this is a loooooooooooooooooooong comment
+      (qwer_tyuiopa_zxcvbnmasdfg)-[:abcdefgh]->(qwertyu),
+      (qwertyu)-[:HIJKLMN_OP]->(asdfghj_klzxcvbnmop),
+      (asdfghj_klzxcvbnmop)-[:QRSTUVWX]->(qazwsxedc_rfvgt),
+      (mnbvcxzasdfghj_poiuytrewq)-[:YZABCDF]->(qwertyu),
+      (mnbvcxzasdfghj_poiuytrewq)-[:GHIJKLMN]->(zxcvbnmlkjhgfd_asdfjkl),
+      (zxcvbnmlkjhgfd_asdfjkl)-[:OPQRS_TU]->(qwertyu),
+      (qwert_yuiopasdfg)-[:OPQRS_TU]->(qwertyu),
+      // this is a loooooooooooooooooooong comment
+      (hjklmno)-[:OPQRS_TU]->(zxcvbn_mnb_lkjhgfdsa),
+      (zxcvbn_mnb_lkjhgfdsa)-[:OPQRS_TU]->(poiuzxcv),
+      (poiuzxcv)-[:OPQRS_TU]->(asdfghjk_qwe),
+      (asdfghjk_qwe)-[:OPQRS_TU]->(zxcvbnmop),
+      (zxcvbnmop)-[:OPQRS_TU]->(qwertyu),
+      (zxcvbnmop)-[:VWXYZABC]->(qwertyuiopa_sdfghjklz),
+      // this is a loooooooooooooooooooong comment
+      (mnbvcxzlkj)-[:VWXYZABC]->(asdfg_hjkltyui),
+      (mnbvcxzlkj)-[:VWXYZABC]->(qwertyuiopa_sdfghjklz),
+      // this is a loooooooooooooooooooong comment
+      (mnbvcxzasdfghj_poiuytrewq)-[:YZABCDF]->(asdfghj_klzxcvbnmop),
+      (mnbvcxzasdfghj_poiuytrewq)-[:DEFHIJKL]->(qazwsxedc_rfvgt),
+      (mnbvcxzasdfghj_poiuytrewq)-[:MNOPQRST]->(qwert_yuiopasdfg);`.trimStart();
     verifyFormatting(query, expected);
   });
 });
