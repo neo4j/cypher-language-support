15eada081f46cefe1cf7ecea9dbd4d27c0c5d126
tomasnyberg <tomas.nyberg7335@gmail.com>
2025-03-19 17:17:25 +0100

    add meaningless change to verify happy path

--- packages/language-support/src/formatting/formatting.ts
+++ packages/language-support/src/formatting/formatting.ts
@@ -465,6 +465,7 @@ export class TreePrintVisitor extends CypherCmdParserVisitor<void> {
     this.visitChildren(ctx);
     this.preserveExplicitNewlineAfter(ctx);
   };
+  // Meaningless change
 
   visitUseClause = (ctx: UseClauseContext) => {
     this.visit(ctx.USE());
