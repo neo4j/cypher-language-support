128ff48a0f77ddb538611503a6238334b2a3a389
tomasnyberg <tomas.nyberg7335@gmail.com>
2025-03-28 09:17:29 +0100

    remove avoidbreakbetween after OPTIONS

--- packages/language-support/src/formatting/formatting.ts
+++ packages/language-support/src/formatting/formatting.ts
@@ -742,7 +742,6 @@ export class TreePrintVisitor extends CypherCmdParserVisitor<void> {
   visitCommandOptions = (ctx: CommandOptionsContext) => {
     this.breakLine();
     this.visit(ctx.OPTIONS());
-    this.avoidBreakBetween();
     this.visit(ctx.mapOrParameter());
   };
 
