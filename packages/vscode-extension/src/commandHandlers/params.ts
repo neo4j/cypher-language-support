aab3f8a7e3ffb1172de55acb3709757ecb53025d
Isak <isak.nilsson@neo4j.com>
2025-03-28 13:43:03 +0100

    Updates error/warning to contain info from driver/linting

--- packages/vscode-extension/src/commandHandlers/params.ts
+++ packages/vscode-extension/src/commandHandlers/params.ts
@@ -11,6 +11,7 @@ import {
   Neo4jType,
   serializeTypeAnnotations,
 } from '@neo4j-cypher/schema-poller';
+import { Neo4jError } from 'neo4j-driver';
 import { window } from 'vscode';
 import { getSchemaPoller } from '../contextService';
 import {
@@ -29,10 +30,19 @@ export async function isConnected(): Promise<boolean> {
   return schemaPoller.connection?.healthcheck();
 }
 
-function validateParamInput(paramValue: string, dbSchema: DbSchema): string {
-  const errors = lintCypherQuery(`RETURN ${paramValue}`, dbSchema, true);
+export function validateParamInput(
+  paramValue: string,
+  dbSchema: DbSchema,
+): string {
+  const errors = lintCypherQuery(
+    `RETURN ${paramValue} AS param`,
+    dbSchema,
+    true,
+  );
   if (errors.length > 0) {
-    return 'Invalid param value';
+    return (
+      'Value can not be evaluated: ' + errors.map((e) => e.message).join(' ')
+    );
   }
   return null;
 }
@@ -66,6 +76,7 @@ export async function addParameter(): Promise<void> {
     ignoreFocusOut: true,
     validateInput: (paramValue) => validateParamInput(paramValue, dbSchema),
   });
+
   if (!paramValue) {
     await window.showErrorMessage('Parameter value cannot be empty.');
     return;
@@ -142,9 +153,6 @@ export async function evaluateParam(
       parameters: {},
     });
     const [record] = result.records;
-    if (record === undefined) {
-      await window.showErrorMessage('Parameter evaluation failed.');
-    }
     const resultEntries = Object.values(record.toObject());
     const paramAsNeo4jType = resultEntries[0] as Neo4jType;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
@@ -165,7 +173,13 @@ export async function evaluateParam(
     });
     parametersTreeDataProvider.refresh();
   } catch (e) {
-    await window.showErrorMessage('Wrong format for the parameter.');
+    if (e instanceof Neo4jError) {
+      //If we can get past linting-check with invalid query but still have failing query
+      //when executing, we catch here as a backup
+      await window.showErrorMessage(
+        'Failed to evaluate parameter: ' + e.message,
+      );
+    }
   }
 
   return;
