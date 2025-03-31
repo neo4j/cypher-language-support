a1d0daaab838be9eadc6446dcbc708f5ee4e3176
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2025-03-31 16:17:33 +0200

    update eslint and tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'prettier',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
   ],
   env: {
     browser: true,
@@ -14,8 +15,34 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./**/tsconfig.json', './**/tsconfig.node.json'],
   },
+  settings: {
+    'import/resolver': {
+      // You will also need to install and configure the TypeScript resolver
+      // See also https://github.com/import-js/eslint-import-resolver-typescript#configuration
+      typescript: true,
+      node: true,
+    },
+  },
+  plugins: ['import'],
   rules: {
     '@typescript-eslint/consistent-type-exports': 'error',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        ts: 'always',
+        tsx: 'always',
+      },
+    ],
+    /*  TODO
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        disallowTypeAnnotations: true,
+      },
+    ],
+    */
   },
 };
