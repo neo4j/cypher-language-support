ab01ccd0a32cdeab89c0762412c0bebcb8e2f74c
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2025-04-01 12:12:53 +0200

    update package exports

--- packages/vscode-extension/tests/fixtures/textmate/simple-match.js
+++ packages/vscode-extension/tests/fixtures/textmate/simple-match.js
@@ -1,35 +1,28 @@
-
-const a = /* cypher */`
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const a = /* cypher */ `
     // This is a comment
     MATCH (n:Label) RETURN function(n.property)
-`
-
-const b = /*cypher*/`  
+`;
+const b = /*cypher*/ `  
     // This is a comment
     MATCH (n:Label) RETURN function(n.property)
-`
-
+`;
 const c = `//cypher
     // This is a comment
     MATCH (n:Label) RETURN function(n.property)
-`
-
+`;
 const e = `/* cypher */
     // This is a comment
     MATCH (n:Label) RETURN function(n.property)
-`
-
+`;
 const f = `/*cypher*/
     // This is a comment
     MATCH (n:Label) RETURN function(n.property)
-`
-
-const g = "/*cypher*/ MATCH (n:Label) RETURN function(n.property)"
-
-const h = '/*cypher*/ MATCH (n:Label) RETURN function(n.property)'
-
+`;
+const g = "/*cypher*/ MATCH (n:Label) RETURN function(n.property)";
+const h = '/*cypher*/ MATCH (n:Label) RETURN function(n.property)';
 // This one shouldn't highglight
-const i = "//cypher MATCH (n:Label) RETURN function(n.property)"
-
+const i = "//cypher MATCH (n:Label) RETURN function(n.property)";
 // This one shouldn't highglight
-const j = '//cypher MATCH (n:Label) RETURN function(n.property)'
+const j = '//cypher MATCH (n:Label) RETURN function(n.property)';
+//# sourceMappingURL=simple-match.js.map
