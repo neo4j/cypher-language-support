d36beb549efa64d61d556f9dec2854e6723655d1
Nacho Cordon <nacho.cordon@neotechnology.com>
2025-04-15 16:57:26 +0100

    Speed up codemirror tests

--- packages/react-codemirror/playwright-ct.config.ts
+++ packages/react-codemirror/playwright-ct.config.ts
@@ -10,13 +10,11 @@ export default defineConfig({
   /* Maximum time one test can run for. */
   timeout: 20 * 1000,
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Retry always a couple of times */
+  retries: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
