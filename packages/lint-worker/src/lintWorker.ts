9fd928d17d6547490fb1239b069743431b10b461
Nacho Cordon <nacho.cordon@neotechnology.com>
2025-04-29 15:08:03 +0100

    Uses workspace:* version in the lint-worker

--- packages/lint-worker/src/lintWorker.ts
+++ packages/lint-worker/src/lintWorker.ts
@@ -1,59 +1,26 @@
 import {
-  DbSchema as DbSchemaNew,
-  Neo4jFunction,
-} from '@neo4j-cypher/language-support-new';
-import {
   DbSchema,
-  // lintCypherQuery as _lintCypherQuery,
-  // _internalFeatureFlags,
-  validateSyntax,
-  validateSemantics,
-  Neo4jProcedure,
+  lintCypherQuery as _lintCypherQuery,
+  _internalFeatureFlags,
+  SyntaxDiagnostic,
 } from '@neo4j-cypher/language-support';
 import workerpool from 'workerpool';
 
 export const WorkerURL = new URL('./lintWorker', import.meta.url).href;
 
-function lintCypherQuery(
+export function lintCypherQuery(
   query: string,
-  dbSchema: DbSchemaNew,
-  //featureFlags: { consoleCommands?: boolean; cypher25?: boolean } = {},
-) {
-  //console.log("Entering lintCypherQuery in worker with featureflags", featureFlags)
+  dbSchema: DbSchema,
+  featureFlags: { consoleCommands?: boolean; cypher25?: boolean } = {},
+): SyntaxDiagnostic[] {
   // We allow to override the consoleCommands feature flag
-  // if (featureFlags.consoleCommands !== undefined) {
-  //   _internalFeatureFlags.consoleCommands = featureFlags.consoleCommands;
-  // }
-  // if (featureFlags.cypher25 !== undefined) {
-  //   _internalFeatureFlags.cypher25 = featureFlags.cypher25;
-  // }
-  if (dbSchema.procedures && dbSchema.functions) {
-    const oldSchema: DbSchema = {
-      procedures: dbSchema.procedures['CYPHER 5'] as Record<
-        string,
-        Neo4jProcedure
-      >,
-      functions: dbSchema.functions['CYPHER 5'] as Record<
-        string,
-        Neo4jFunction
-      >,
-      labels: dbSchema.labels,
-      relationshipTypes: dbSchema.relationshipTypes,
-      databaseNames: dbSchema.databaseNames,
-      aliasNames: dbSchema.aliasNames,
-      // userNames: dbSchema.userNames,
-      // roleNames: dbSchema.roleNames,
-      parameters: dbSchema.parameters,
-      propertyKeys: dbSchema.propertyKeys,
-    };
-    const syntaxErrors = validateSyntax(query, oldSchema);
-    const semanticErrors =
-      syntaxErrors.length > 0 ? [] : validateSemantics(query, oldSchema);
-
-    return syntaxErrors.concat(semanticErrors);
+  if (featureFlags.consoleCommands !== undefined) {
+    _internalFeatureFlags.consoleCommands = featureFlags.consoleCommands;
   }
-
-  return validateSyntax(query, {});
+  if (featureFlags.cypher25 !== undefined) {
+    _internalFeatureFlags.cypher25 = featureFlags.cypher25;
+  }
+  return _lintCypherQuery(query, dbSchema);
 }
 
 workerpool.worker({ lintCypherQuery });
