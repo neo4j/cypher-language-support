89f53844014fb384d80e8a3d0878f81fbc948fc3
Isak <isak.nilsson@neo4j.com>
2025-05-21 16:10:07 +0200

    strips serverVersion for zero-pad. Modifies minsuppertedversion for cypher 25

--- packages/language-support/src/version.ts
+++ packages/language-support/src/version.ts
@@ -1,17 +1,21 @@
 import semver from 'semver';
 
 export function cypher25Supported(serverVersion: string) {
-  const minSupportedVersion = '5.27.0-2025040';
+  const minSupportedVersion = '2025.8.0'; //TODO: set to correct value when cypher 25 is actually released
   const minSupported = semver.coerce(minSupportedVersion, {
     includePrerelease: false,
   });
-  const current = semver.coerce(serverVersion, { includePrerelease: false });
+
+  const cleanedServerVersion = serverVersion.replace(/"(\.0+)(?=\d)"/g, '.');
+  const current = semver.coerce(cleanedServerVersion, {
+    includePrerelease: false,
+  });
 
   if (minSupported && current) {
     const comparison = semver.compare(minSupported, current);
     if (comparison === 0) {
       const minPrelease = semver.prerelease(minSupportedVersion)?.at(0);
-      const currentPrelease = semver.prerelease(serverVersion)?.at(0);
+      const currentPrelease = semver.prerelease(cleanedServerVersion)?.at(0);
 
       return (
         typeof minPrelease === 'number' &&
