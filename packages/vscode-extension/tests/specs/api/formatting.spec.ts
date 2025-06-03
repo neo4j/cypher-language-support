8635dd347708f00f028217ab13067faa0a1c1a54
Isak <isak.nilsson@neo4j.com>
2025-06-03 16:15:34 +0200

    removes needless formatting test workaround, updates lockfile

--- packages/vscode-extension/tests/specs/api/formatting.spec.ts
+++ packages/vscode-extension/tests/specs/api/formatting.spec.ts
@@ -2,8 +2,6 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import { newUntitledFileWithContent } from '../../helpers';
 
-const normalizeLineEndings = (str: string) => str.replace(/\r\n/g, '\n');
-
 suite('Formatting', () => {
   test('tests that formatting document works', async () => {
     const query = `match (p:   Person)  where  p.name = "John Doe" reTUrn p lIMIt 25`;
@@ -14,9 +12,6 @@ suite('Formatting', () => {
 WHERE p.name = "John Doe"
 RETURN p
 LIMIT 25`;
-    assert.equal(
-      normalizeLineEndings(formattedText),
-      normalizeLineEndings(expected),
-    );
+    assert.equal(formattedText, expected);
   });
 });
