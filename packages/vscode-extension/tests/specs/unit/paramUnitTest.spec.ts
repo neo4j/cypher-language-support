3f6241d19e611f46c1d943dd72067e75724e296d
Nacho Cordon <nacho.cordon@neotechnology.com>
2025-06-05 14:38:50 +0100

    Fixes tests not working

--- packages/vscode-extension/tests/specs/unit/paramUnitTest.spec.ts
+++ packages/vscode-extension/tests/specs/unit/paramUnitTest.spec.ts
@@ -27,7 +27,7 @@ suite('Parameter validation spec', () => {
   test('Parameter validation fails for incorrect inputs', () => {
     assert.strictEqual(
       validateParamInput('datetime(', dbSchema),
-      "Value cannot be evaluated: Invalid input '(': expected an expression, ',', 'AS', 'ORDER BY', 'CALL', 'CREATE', 'LOAD CSV', 'DELETE', 'DETACH', 'FINISH', 'FOREACH', 'INSERT', 'LIMIT', 'MATCH', 'MERGE', 'NODETACH', 'OFFSET', 'OPTIONAL', 'REMOVE', 'RETURN', 'SET', 'SKIP', 'UNION', 'UNWIND', 'USE', 'WITH' or <EOF>",
+      "Value cannot be evaluated: Variable `datetime` not defined. Invalid input '(': expected an expression, ',', 'AS', 'ORDER BY', 'CALL', 'CREATE', 'LOAD CSV', 'DELETE', 'DETACH', 'FINISH', 'FOREACH', 'INSERT', 'LIMIT', 'MATCH', 'MERGE', 'NODETACH', 'OFFSET', 'OPTIONAL', 'REMOVE', 'RETURN', 'SET', 'SKIP', 'UNION', 'UNWIND', 'USE', 'WITH' or <EOF>",
     );
     assert.strictEqual(
       validateParamInput('500q', dbSchema),
