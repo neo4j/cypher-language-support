578ad939367d0a04d6af738f0dd50e81222a17a4
Isak <isak.nilsson@neo4j.com>
2025-06-18 17:07:22 +0200

    add test to make sure valid positions are not modified

--- packages/language-support/src/tests/misc.test.ts
+++ packages/language-support/src/tests/misc.test.ts
@@ -6,19 +6,19 @@ import {
 import { DiagnosticSeverity, Position } from 'vscode-languageserver-types';
 
 describe('Clean positions', () => {
-  test('Cleaning positions should mark the entire document', () => {
+  test('Cleaning positions should mark the entire document when a position is negative.', () => {
     let error: SyntaxDiagnostic;
-    error = makeBadDiagnostic(Position.create(0, 0), Position.create(0, -5), {
+    error = makeDiagnostic(Position.create(0, 0), Position.create(0, -5), {
       start: 0,
       end: 20,
     });
     testPositionCoversDoc('abcdefg \nblablbla \nbc', 3, 2, error);
-    error = makeBadDiagnostic(Position.create(0, 0), Position.create(0, 4), {
+    error = makeDiagnostic(Position.create(0, 0), Position.create(0, 4), {
       start: 0,
       end: -1,
     });
     testPositionCoversDoc('1234', 1, 4, error);
-    error = makeBadDiagnostic(Position.create(-3, 0), Position.create(0, 5), {
+    error = makeDiagnostic(Position.create(-3, 0), Position.create(0, 5), {
       start: 0,
       end: 5,
     });
@@ -28,6 +28,22 @@ describe('Clean positions', () => {
       17,
       error,
     );
+  });
+
+  test('Cleaning positions should not make any change when position is valid.', () => {
+    const error = makeDiagnostic(Position.create(0, 0), Position.create(0, 3), {
+      start: 0,
+      end: 3,
+    });
+    const text = 'abcdefg \n ababab';
+    const textDoc: TextDocument = TextDocument.create(
+      'testDoc.cypher',
+      'cypher',
+      0,
+      text,
+    );
+    const cleanedError = clampUnsafePositions([error], textDoc)[0];
+    expect(cleanedError).toBe(error);
   });
 });
 
@@ -53,7 +69,7 @@ function testPositionCoversDoc(
   expect(cleanedResult.offsets.end).toBe(text.length);
 }
 
-const makeBadDiagnostic = (
+const makeDiagnostic = (
   start: Position,
   end: Position,
   offsets: { start: number; end: number },
