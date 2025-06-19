3bf984e26c30e48fc842e778528615b97de5145e
Isak <isak.nilsson@neo4j.com>
2025-06-19 09:16:38 +0200

    minor changes to helper

--- packages/lint-worker/src/helpers.ts
+++ packages/lint-worker/src/helpers.ts
@@ -1,16 +1,20 @@
 import { compareMajorMinorVersions } from './version';
 import {
-  DbSchema as DbSchema2,
+  DbSchema as DbSchemaV2,
   Neo4jFunction,
   Neo4jProcedure,
 } from '@neo4j-cypher/language-support';
 import { Neo4jSchemaPoller } from '@neo4j-cypher/query-tools';
-import { DbSchema as DbSchema1 } from 'languageSupport-next.13';
+import { DbSchema as DbSchemaV1 } from 'languageSupport-next.13';
 
+const oldLinter = '5.20.0';
+
+// for older versions of the language support, the dbschema was not the same,
+// meaning old linters need conversion of the new schema
 export function convertDbSchema(
-  originalSchema: DbSchema2,
+  originalSchema: DbSchemaV2,
   neo4j: Neo4jSchemaPoller,
-): DbSchema2 | DbSchema1 {
+): DbSchemaV2 | DbSchemaV1 {
   let oldFunctions: Record<string, Neo4jFunction> = {};
   let oldProcedures: Record<string, Neo4jProcedure> = {};
   if (!originalSchema) {
@@ -27,8 +31,8 @@ export function convertDbSchema(
   const serverVersion = neo4j.connection?.serverVersion;
   const linterVersion = serverVersionToLinter(serverVersion);
 
-  if (compareMajorMinorVersions(linterVersion, '5.21.0') < 0) {
-    const dbSchemaOld: DbSchema1 = {
+  if (compareMajorMinorVersions(linterVersion, oldLinter) < 0) {
+    const dbSchemaOld: DbSchemaV1 = {
       ...originalSchema,
       functions: oldFunctions,
       procedures: oldProcedures,
@@ -40,8 +44,6 @@ export function convertDbSchema(
 }
 
 export function serverVersionToLinter(serverVersion: string) {
-  const oldLinter = '5.20.0';
-
   let candidate: string = undefined;
   if (compareMajorMinorVersions(serverVersion, oldLinter) <= 0) {
     candidate = oldLinter;
