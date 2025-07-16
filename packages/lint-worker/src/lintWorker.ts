27d9d5003c5b202a101c0a5c0aa8411652cf107a
Nacho Cordon <nacho.cordon@neotechnology.com>
2025-07-16 15:10:07 +0100

    2025.04 linter release

--- packages/lint-worker/src/lintWorker.ts
+++ packages/lint-worker/src/lintWorker.ts
@@ -6,7 +6,7 @@ import {
   DbSchema,
   lintCypherQuery as _lintCypherQuery,
   _internalFeatureFlags,
-} from 'languageSupport-next.17.5';
+} from 'languageSupport-next.20';
 import workerpool from 'workerpool';
 
 function lintCypherQuery(
