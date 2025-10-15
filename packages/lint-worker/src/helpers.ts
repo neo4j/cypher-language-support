6b70433337fcd5f04eb0b20d00496bc6fbd2e5ac
Isak <isak.nilsson@neo4j.com>
2025-10-15 11:21:09 +0200

    fixes so linterFileToServerVersion can handle versions like <serverVersion>-lintWorker-<version><anything>.cjs, supporting the linters released via changesets updates to versions

--- packages/lint-worker/src/helpers.ts
+++ packages/lint-worker/src/helpers.ts
@@ -65,7 +65,7 @@ export function serverVersionToLinter(serverVersion: string): {
 }
 
 export function linterFileToServerVersion(fileName: string) {
-  const linterFileRegex = /^([\d.]+)-lintWorker-([\d.]+)\.cjs$/;
+  const linterFileRegex = /^([\d.]+)-lintWorker-([\d.]+).*?\.cjs$/;
   return fileName ? fileName.match(linterFileRegex)?.[1] : undefined;
 }
 
