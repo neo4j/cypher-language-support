671e0ea990c09e6ba4e3e0df45d7852eeb59787d
Isak Nilsson <143806924+anderson4j@users.noreply.github.com>
2025-12-19 10:01:11 +0100

    Refetch schema if db changes from query execution (#626)

--- packages/vscode-extension/src/webviews/queryResults/queryDetailsProvider.ts
+++ packages/vscode-extension/src/webviews/queryResults/queryDetailsProvider.ts
@@ -94,6 +94,24 @@ export class Neo4jQueryDetailsProvider implements WebviewViewProvider {
   private async executeStatement(statement: string) {
     const webview = this.view.webview;
     const result = await this.runQuery(statement);
+    if ('summary' in result) {
+      const containsUpdates = result.summary.counters.containsUpdates();
+      const containsSystemUpdates =
+        result.summary.counters.containsSystemUpdates();
+      const isSupportedDatabase =
+        this.schemaPoller.connection?.currentDb !== 'system';
+      const couldbeLoadingDataWithApoc = result.summary.query.text
+        .toLowerCase()
+        .includes('apoc');
+      if (
+        (containsUpdates ||
+          containsSystemUpdates ||
+          couldbeLoadingDataWithApoc) &&
+        isSupportedDatabase
+      ) {
+        this.schemaPoller.metadata?.fetchDbSchema();
+      }
+    }
     const message: QueryResultsMessage = {
       type: 'executionUpdate',
       to: 'detailsView',
