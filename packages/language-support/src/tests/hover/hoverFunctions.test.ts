978df1c60ad814383d3909eb916fe9043cd3997a
angrykoala <angrykoala@outlook.es>
2026-09-02 16:23:08 +0200

    update tests to use mock schema

--- packages/language-support/src/tests/hover/hoverFunctions.test.ts
+++ packages/language-support/src/tests/hover/hoverFunctions.test.ts
@@ -1,62 +1,27 @@
-import { DbSchema } from '../../dbSchema.js';
 import { CypherLanguageService } from '../../cypherLanguageService.js';
 import { testData } from '../testData.js';
 
 describe('Functions hover', () => {
   test('provides hover info for functions', () => {
-    const query = 'CYPHER 25 RETURN custom.subtract(1, 2), custom.add(1, 2)';
-    const dbSchema: DbSchema = {
-      functions: {
-        'CYPHER 25': {
-          'custom.add': {
-            name: 'custom.add',
-            category: '',
-            description: 'Adds two numbers.',
-            isBuiltIn: false,
-            argumentDescription: [
-              {
-                name: 'left',
-                description: 'The left number.',
-                isDeprecated: false,
-                type: 'INTEGER',
-              },
-            ],
-            returnDescription: 'INTEGER',
-            signature: 'custom.add(left :: INTEGER) :: INTEGER',
-            aggregating: false,
-            isDeprecated: false,
-          },
-          'custom.subtract': {
-            name: 'custom.subtract',
-            category: '',
-            description: 'Subtracts two numbers.',
-            isBuiltIn: false,
-            argumentDescription: [],
-            returnDescription: 'INTEGER',
-            signature: 'custom.subtract(left :: INTEGER) :: INTEGER',
-            aggregating: false,
-            isDeprecated: false,
-          },
-        },
-      },
-    };
+    const query = 'CYPHER 25 RETURN abs(1,2)';
 
     const hoverInfo = new CypherLanguageService().hoverInfo(query, {
-      caretPosition: query.indexOf('custom.add') + 1,
-      dbSchema,
+      caretPosition: query.indexOf('abs') + 1,
+      dbSchema: testData.mockSchema,
     });
 
     expect(hoverInfo).toStrictEqual({
-      signature: 'custom.add(left :: INTEGER) :: INTEGER',
-      description: 'Adds two numbers.',
-      returnDescription: 'INTEGER',
+      signature: 'abs(input :: INTEGER | FLOAT) :: INTEGER | FLOAT',
+      description: 'Returns the absolute value of an `INTEGER` or `FLOAT`.',
+      returnDescription: 'INTEGER | FLOAT',
       isDeprecated: false,
       params: [
         {
-          name: 'left',
-          description: 'The left number.',
+          name: 'input',
+          description:
+            'A numeric value from which the absolute number will be returned.',
           isDeprecated: false,
-          type: 'INTEGER',
+          type: 'INTEGER | FLOAT',
         },
       ],
     });
--- packages/language-support/src/tests/hover/hoverProcedures.test.ts
+++ packages/language-support/src/tests/hover/hoverProcedures.test.ts
@@ -1,77 +1,29 @@
-import { DbSchema } from '../../dbSchema.js';
 import { CypherLanguageService } from '../../cypherLanguageService.js';
+import { testData } from '../testData.js';
 
 describe('Procedures hover', () => {
   test('provides hover info for procedures', () => {
-    const query = "CYPHER 25 CALL custom.other() CALL custom.proc('x')";
-    const dbSchema: DbSchema = {
-      procedures: {
-        'CYPHER 25': {
-          'custom.proc': {
-            name: 'custom.proc',
-            description: 'Runs a custom procedure.',
-            mode: 'READ',
-            worksOnSystem: false,
-            argumentDescription: [
-              {
-                name: 'input',
-                description: 'The input value.',
-                isDeprecated: false,
-                type: 'STRING',
-              },
-            ],
-            returnDescription: [
-              {
-                name: 'result',
-                description: 'The result value.',
-                isDeprecated: false,
-                type: 'STRING',
-              },
-            ],
-            signature: 'custom.proc(input :: STRING) :: (result :: STRING)',
-            admin: false,
-            option: { deprecated: false },
-          },
-          'custom.other': {
-            name: 'custom.other',
-            description: 'Runs another procedure.',
-            mode: 'READ',
-            worksOnSystem: false,
-            argumentDescription: [],
-            returnDescription: [],
-            signature: 'custom.other() :: ()',
-            admin: false,
-            option: { deprecated: false },
-          },
-        },
-      },
-    };
+    const query = 'CALL db.labels()';
 
     const hoverInfo = new CypherLanguageService().hoverInfo(query, {
-      caretPosition: query.indexOf('custom.proc') + 1,
-      dbSchema,
+      caretPosition: query.indexOf('db.labels') + 1,
+      dbSchema: testData.mockSchema,
     });
 
     expect(hoverInfo).toStrictEqual({
-      signature: 'custom.proc(input :: STRING) :: (result :: STRING)',
-      description: 'Runs a custom procedure.',
+      signature: 'db.labels() :: (label :: STRING)',
+      description:
+        "List all labels attached to nodes within a database according to the user's access rights. The procedure returns empty results if the user is not authorized to view those labels.",
       returnDescription: [
         {
-          name: 'result',
-          description: 'The result value.',
+          name: 'label',
+          description: 'A label within the database.',
           isDeprecated: false,
           type: 'STRING',
         },
       ],
       isDeprecated: false,
-      params: [
-        {
-          name: 'input',
-          description: 'The input value.',
-          isDeprecated: false,
-          type: 'STRING',
-        },
-      ],
+      params: [],
     });
   });
 });
