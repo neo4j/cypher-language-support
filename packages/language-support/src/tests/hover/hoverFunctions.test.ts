f2901d5c204469281a23626b3bf1e8c73b3e7a43
angrykoala <angrykoala@outlook.es>
2026-09-02 15:55:43 +0200

    add tests on nested functions hover

--- packages/language-support/src/tests/hover/hoverFunctions.test.ts
+++ packages/language-support/src/tests/hover/hoverFunctions.test.ts
@@ -86,4 +86,49 @@ describe('Functions hover', () => {
       ],
     });
   });
+
+  test('provides hover info for functions wrapping functions', () => {
+    const query = 'MATCH (n) RETURN abs(count(n))';
+
+    const hoverInfo = new CypherLanguageService().hoverInfo(query, {
+      caretPosition: query.indexOf('abs') + 1,
+      dbSchema: testData.mockSchema,
+    });
+
+    const innerHoverInfo = new CypherLanguageService().hoverInfo(query, {
+      caretPosition: query.indexOf('count') + 1,
+      dbSchema: testData.mockSchema,
+    });
+
+    expect(hoverInfo).toStrictEqual({
+      signature: 'abs(input :: INTEGER | FLOAT) :: INTEGER | FLOAT',
+      description: 'Returns the absolute value of an `INTEGER` or `FLOAT`.',
+      returnDescription: 'INTEGER | FLOAT',
+      isDeprecated: false,
+      params: [
+        {
+          name: 'input',
+          description:
+            'A numeric value from which the absolute number will be returned.',
+          isDeprecated: false,
+          type: 'INTEGER | FLOAT',
+        },
+      ],
+    });
+
+    expect(innerHoverInfo).toStrictEqual({
+      signature: 'count(input :: ANY) :: INTEGER',
+      description: 'Returns the number of values or rows.',
+      returnDescription: 'INTEGER',
+      isDeprecated: false,
+      params: [
+        {
+          name: 'input',
+          description: 'A value to be aggregated.',
+          isDeprecated: false,
+          type: 'ANY',
+        },
+      ],
+    });
+  });
 });
