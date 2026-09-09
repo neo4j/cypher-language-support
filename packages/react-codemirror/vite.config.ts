fc7bdf743a2bf1d004e5162f9a6ae19f9262d7fb
angrykoala <angrykoala@outlook.es>
2026-09-09 15:59:26 +0100

    Make tsconfig consistent across packages and fix type errors (#745)
    * make tsconfig consistent across packages and fix some type errors
    
    * small tweaks to tsconfig
    
    * code review changes
    
    * Fix merge changes
    
    ---------
    
    Co-authored-by: Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>

--- packages/react-codemirror/vite.config.ts
+++ packages/react-codemirror/vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react';
-import { configDefaults, defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
