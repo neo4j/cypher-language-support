980c4c134d098614ccb91011a63d1204bfa6ca59
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2026-09-10 16:22:27 +0200

    Simplify type cast
    Co-authored-by: angrykoala <angrykoala@outlook.es>

--- packages/react-codemirror/src/CypherEditor.test.tsx
+++ packages/react-codemirror/src/CypherEditor.test.tsx
@@ -18,9 +18,7 @@ const onChange = vi.fn((v: string) => {
   rerender();
 });
 
-(
-  globalThis as typeof globalThis & { IS_REACT_ACT_ENVIRONMENT: boolean }
-).IS_REACT_ACT_ENVIRONMENT = true;
+(global as any).IS_REACT_ACT_ENVIRONMENT = true;
 const DEBOUNCE_TIME_WITH_MARGIN = DEBOUNCE_TIME + 100;
 
 /** Avoids crash in test environment */
